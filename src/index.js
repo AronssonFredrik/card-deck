2d7a44f59828a3ca1f1b52bdaa1c4342144f281e
Fredrik Aronsson <Fredrik.Aronsson@sigma.se>
2018-11-12 13:34:43 +0100

    update script

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-// use quokka.js for easy debug
-
 // create deck
 const suits = ["hearts", "diamonds", "spades", "suits"];
 const ranks = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"];
-let deck = [];
-let shuffled = [];
+let deck = [], shuffled = [], cardPicks = [], winningOrder = [], choice = '';
+
+let winningAmount = 0,
+    testRuns = 0;
 
 for (rank in ranks) {
   for (suit in suits) {
     deck.push({
       suit: suits[suit],
       rank: ranks[rank],
-      name: `${ranks[rank]} of ${suits[suit]}`
+      name: `${ranks[rank]} of ${suits[suit]}`,
+      worth: Number(rank) + ((Number(suit) +1) * .1)
     });
   }
 }
+
 // sort deck function
 function shuffle(deck) {
   shuffled = deck;
@@ -25,15 +27,56 @@ function shuffle(deck) {
     shuffled[next] = swap;
   }
 }
+
 // pick 3 deck function
 function pickThreeCards() {
-  let picks = [];
-  picks.push(shuffled[0], shuffled[1], shuffled[2]);
-  for (pick in picks) picks[pick].order = pick;
-  console.log(picks);
+  cardPicks = [];
+  cardPicks.push(shuffled[0], shuffled[1], shuffled[2]);
 }
 
-// run shuffle
-shuffle(deck);
-// run pick
-pickThreeCards();
+// find winning hand
+function winningHand() {
+  winningOrder = []
+  for(card in cardPicks)  winningOrder.push(cardPicks[card])
+  winningOrder.sort(function(a,b){ return a.worth - b.worth })
+}
+// make a first pick
+function makePick(pickVal) {
+  if (pickVal === 1)                // hardcode first pick
+  choice = cardPicks[1]
+  else if (pickVal === 2)           // hardcode second pick to next possible
+    choice === cardPicks[1] 
+      ? choice = cardPicks[0]
+      : choice = cardPicks[0]
+}
+// remove random card which is not winner and not picked
+function removeCard() {  
+  do {
+    let randomCard = Math.floor(Math.random() * (cardPicks.length))
+    cardPicks[randomCard] !== choice && cardPicks[randomCard] !== winningOrder[0]
+      ? cardPicks.pop(randomCard) : null
+  }while(cardPicks.length < 2)
+}
+
+// Was it a winner ?
+function winnerVerdict() {
+  choice === winningOrder[0] ? winningAmount++ : null 
+  testRuns++
+}
+// return win chance
+function winChance() {
+  console.log(`${testRuns} test has run. With ${winningAmount} wins. A chance of ${winningAmount/testRuns * 100} %.`)
+}
+
+/*    ###  Exectuion of fubnctions ###    */
+while(testRuns < 1000) {
+  shuffle(deck);
+  pickThreeCards();
+  winningHand()
+  makePick(1)
+  removeCard()
+  makePick(2)
+  winnerVerdict()
+}
+winChance()
+console.log(deck.length)
